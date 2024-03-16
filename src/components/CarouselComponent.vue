094c1ec4d02ef484a8f00587cc9ab3d73cc332b0
aggeB8 <brannmarkdev@gmail.com>
2024-03-16 04:46:48 +0100

    +working carousel

--- src/components/CarouselComponent.vue
+++ src/components/CarouselComponent.vue
@@ -1,35 +1,55 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import lsActions from '@/composables/localstorage'
+import { onMounted, onUnmounted, ref } from 'vue'
 
 const carouselContainer = ref()
 const carouselAnimClass = ref()
-const carouselDupe = ref(1)
+const carouselDupe = ref(2)
+
+const switchCarousel = () => {
+    switch (carouselDupe.value) {
+        case 1: {
+            console.log('1', carouselContainer.value.scrollWidth)
+            if (carouselContainer.value.scrollWidth > window.innerWidth) {
+                carouselAnimClass.value = 'animate'
+                carouselDupe.value = 2
+            }
+            break
+        }
+        case 2: {
+            const actualCarouselWidth = carouselContainer.value.scrollWidth / 2
+            if (actualCarouselWidth < window.innerWidth) {
+                console.log('2', carouselContainer.value.scrollWidth / 2, window.innerWidth)
+
+                carouselAnimClass.value = ''
+                carouselDupe.value = 1
+            }
+            break
+        }
+    }
+}
 
 onMounted(() => {
-    if (carouselContainer.value.scrollWidth > window.innerWidth) {
-        carouselAnimClass.value = 'animate'
-        carouselDupe.value = 2
-    } else {
-        carouselAnimClass.value = ''
-        carouselDupe.value = 1
-    }
+    switchCarousel()
+    window.addEventListener('resize', switchCarousel)
+})
+
+onUnmounted(() => {
+    window.removeEventListener('resize', switchCarousel)
 })
 </script>
 
 <template>
-    <div
-        ref="carouselContainer"
-        class="flex h-40 max-w-full justify-center overflow-hidden bg-slate-800"
-    >
-        <div v-for="i in carouselDupe" :key="i" ref="carousel" class="flex">
-            <div
-                v-for="i in 4"
-                :key="i"
-                :class="carouselAnimClass"
-                class="h-full w-40 flex-shrink-0 px-2"
-            >
-                <div class="h-full w-full bg-slate-400"></div>
+    <div class="flex h-40 max-w-full justify-center bg-slate-800">
+        <div ref="carouselContainer" class="flex">
+            <div v-for="i in carouselDupe" :key="i" class="flex flex-row flex-nowrap">
+                <div
+                    v-for="i in 4"
+                    :key="i"
+                    :class="carouselAnimClass"
+                    class="h-full w-40 flex-shrink-0 px-2"
+                >
+                    <div class="h-full w-full bg-slate-400"></div>
+                </div>
             </div>
         </div>
     </div>
